3b929c534b38d069be7b44fa09d8dd3d78e9f481
hackpupu <kdh@i2sec.co.kr>
2018-08-09 02:55:10 -0700

    Error page added

--- app/__init__.py
+++ app/__init__.py
@@ -24,6 +24,10 @@ def index():
 def not_found(error):
     return render_template('404.html'), 404
 
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
+
 # Blueprint Register section
 from app.dashboard.views import mod as dashboardModule
 app.register_blueprint(dashboardModule)
--- config.py
+++ config.py
@@ -9,6 +9,7 @@ SECRET_KEY = os.urandom(24)
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'app.db')
 DATABASE_CONNECT_OPTIONS = {}
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 THREADS_PER_PAGE = 8
 
