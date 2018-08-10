e691defbe4e0e6582e2fbbe839e05eae1ed0639c
hackpupu <kdh@i2sec.co.kr>
2018-08-10 04:39:57 -0700

    Update Prototype open

--- config.py
+++ config.py
@@ -1,14 +1,18 @@
 import os
+from configparser import ConfigParser
+
+config = ConfigParser()
+config.read('secret_config.ini')
 _basedir = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = False
 
-ADMINS = frozenset(['support@reponse.co'])
 # SECRET_KEY = '{{ REPLACE THIS}}'
 SECRET_KEY = os.urandom(24)
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'app.db')
-DATABASE_CONNECT_OPTIONS = {}
+if DEBUG:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(_basedir, 'app.db')
+else:
+    SQLALCHEMY_DATABASE_URI = config['GCLOUD']['SQLALCHEMY_DATABASE_URI']
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 THREADS_PER_PAGE = 8
--- main.py
+++ main.py
@@ -2,10 +2,8 @@ from pathlib import Path
 from app import app
 
 if __name__ == '__main__':
-    db_file = Path('./app.db')
-    if not db_file.is_file():
-        from app import db
-        from producer_insert_from_json import insert_producer
-        db.create_all()
-        insert_producer()
+    from app import db
+    from utils.producer_insert_from_json import insert_producer
+    db.create_all()
+    insert_producer()
     app.run(host='0.0.0.0', port=8080, debug=True)
